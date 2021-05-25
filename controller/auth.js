ec1fd7a35a023e2bc8f87ef1bdfd115611193385
BULENT KAYICI <bulentkyc@MacBook-Pro.fritz.box>
2021-05-25 12:19:14 +0200

    v7.1

---
+++ controller/auth.js
@@ -0,0 +1,34 @@
+const users = require('../model/users');
+
+exports.registerPost = (req, res) => {
+/* 
+    const email = req.body.email;
+    const pass = req.body.pass;
+
+    const {email, pass} = req.body;
+
+    const newUser = new users({email, pass});
+ */
+    
+    const newUser = new users(req.body);
+
+    users.findOne({email:req.body.email}, (err, doc) => {
+        if (err) {
+            console.log(err);
+            res.send({status:'failed', message: err});
+        } else if(doc !== null) {
+            res.status(406).send(({status:'failed', message: 'Please try different email!'}));
+        } else {
+            newUser.save((err, doc)=>{
+                if (err) {
+                    console.log(err);
+                    res.send({status:'failed', message: err});
+                } else {
+                    console.log(doc);
+                    res.send(({status:'success', message: 'The account is created successfully'}));
+                }
+            });
+        }
+    });
+
+}
