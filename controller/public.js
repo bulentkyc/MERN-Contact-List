56afd9f25cfd86c8350ceeec30dffc122b73c071
BULENT KAYICI <bulentkyc@MacBook-Pro.fritz.box>
2021-06-09 09:59:52 +0200

    v11.1

--- controller/public.js
+++ controller/public.js
@@ -26,49 +26,58 @@ exports.about = (req, res) => {
     });
 }
 
-exports.getContact = async (req, res) => {
-    await upload(req, res, (err)=>{
+exports.getContact =  (req, res) => {
+    upload(req, res, async (err)=>{
         if (err) {
             console.log(err);
         }
+        console.log('files', req.files);
         console.log(req.body, req.files);
+        try {
+            //let testAccount = await nodemailer.createTestAccount();
+
+            // create reusable transporter object using the default SMTP transport
+            let transporter = nodemailer.createTransport({
+                service: 'Gmail', // no need to set host or port etc.
+                auth: {
+                    user: 'bulentkyc@gmail.com',
+                    pass: process.env.GMAIL
+                }
+            });
+
+            // console.log({
+            //     user: testAccount.user, // generated ethereal user
+            //     pass: testAccount.pass, // generated ethereal password
+            // });
+
+            // send mail with defined transport object
+            let info = await transporter.sendMail({
+                from: '"Customer Support" <visitor@mywebsite.com>', // sender address
+                to: "bulent.kayici@icloud.com, nikolas.dimitriadis@hotmail.com", // list of receivers
+                subject: "Ticket from: "+req.body.fullName, // Subject line
+                text: req.body.message, // plain text body
+                html: `<b>${req.body.message}</b>`, // html body
+                //Attach: attachments/f-1623223770724.png
+                attachments: [
+                    {   // utf-8 string as an attachment
+                        filename: 'test.png',
+                        path: 'attachments/f-1623223770724.png'//req.files[0].path
+                    }
+                ]
+
+            });
+
+            console.log("Message sent: %s", info.messageId);
+            res.json({status: 'success', message: 'Congratulations!'});
+            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+            // Preview only available when sending through an Ethereal account
+            //console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        } catch (err) {
+            console.log(err)
+            res.status(401).json({status:'failed', message:err});
+        }
     });
 
-    console.log(req.body, req.files);
-    /* try {
-        //let testAccount = await nodemailer.createTestAccount();
-
-        // create reusable transporter object using the default SMTP transport
-        let transporter = nodemailer.createTransport({
-            service: 'Gmail', // no need to set host or port etc.
-            auth: {
-                user: 'bulentkyc@gmail.com',
-                pass: process.env.GMAIL
-            }
-        });
-
-        // console.log({
-        //     user: testAccount.user, // generated ethereal user
-        //     pass: testAccount.pass, // generated ethereal password
-        // });
-
-        // send mail with defined transport object
-        let info = await transporter.sendMail({
-            from: '"Customer Support" <visitor@mywebsite.com>', // sender address
-            to: "bulent.kayici@icloud.com, nikolas.dimitriadis@hotmail.com", // list of receivers
-            subject: "Ticket from: "+req.body.fullName, // Subject line
-            text: req.body.message, // plain text body
-            html: `<b>${req.body.message}</b>`, // html body
-        });
-
-        console.log("Message sent: %s", info.messageId);
-        res.json({status: 'success', message: 'Congratulations!'});
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-        // Preview only available when sending through an Ethereal account
-        //console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    } catch (err) {
-        console.log(err)
-        res.status(401).json({status:'failed', message:err});
-    } */
+    
 }
