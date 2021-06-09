16531f096744a4b5e9926f92efae4e3c93e92165
BULENT KAYICI <bulentkyc@MacBook-Pro.fritz.box>
2021-06-09 09:24:02 +0200

    v11

--- controller/public.js
+++ controller/public.js
@@ -1,5 +1,20 @@
 const contacts = require('../model/contacts');
 const nodemailer = require("nodemailer");
+var multer  = require('multer');
+const path = require('path');
+const { log } = require('console');
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'attachments')
+    },
+    filename: function (req, file, cb) {
+        console.log(123, file);
+        cb(null, 'f' + '-' + Date.now() + path.extname(file.originalname))
+    }
+});
+
+const upload = multer({ storage: storage }).array('attachs');
 
 exports.about = (req, res) => {
     contacts.find({},(err, docs)=>{
@@ -12,37 +27,48 @@ exports.about = (req, res) => {
 }
 
 exports.getContact = async (req, res) => {
-    console.log(req.body);
-    res.json({status: 'success', message: 'Congratulations!'});
-
-    //let testAccount = await nodemailer.createTestAccount();
-
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        service: 'Gmail', // no need to set host or port etc.
-        auth: {
-            user: 'bulentkyc@gmail.com',
-            pass: process.env.GMAIL
+    await upload(req, res, (err)=>{
+        if (err) {
+            console.log(err);
         }
+        console.log(req.body, req.files);
     });
 
-    /* console.log({
-        user: testAccount.user, // generated ethereal user
-        pass: testAccount.pass, // generated ethereal password
-    }); */
+    console.log(req.body, req.files);
+    /* try {
+        //let testAccount = await nodemailer.createTestAccount();
 
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-        from: '"Customer Support" <visitor@mywebsite.com>', // sender address
-        to: "bulent.kayici@icloud.com, nikolas.dimitriadis@hotmail.com", // list of receivers
-        subject: "Ticket from: "+req.body.fullName, // Subject line
-        text: req.body.message, // plain text body
-        html: `<b>${req.body.message}</b>`, // html body
-    });
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+            service: 'Gmail', // no need to set host or port etc.
+            auth: {
+                user: 'bulentkyc@gmail.com',
+                pass: process.env.GMAIL
+            }
+        });
 
-    console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+        // console.log({
+        //     user: testAccount.user, // generated ethereal user
+        //     pass: testAccount.pass, // generated ethereal password
+        // });
 
-    // Preview only available when sending through an Ethereal account
-    //console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+            from: '"Customer Support" <visitor@mywebsite.com>', // sender address
+            to: "bulent.kayici@icloud.com, nikolas.dimitriadis@hotmail.com", // list of receivers
+            subject: "Ticket from: "+req.body.fullName, // Subject line
+            text: req.body.message, // plain text body
+            html: `<b>${req.body.message}</b>`, // html body
+        });
+
+        console.log("Message sent: %s", info.messageId);
+        res.json({status: 'success', message: 'Congratulations!'});
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+        // Preview only available when sending through an Ethereal account
+        //console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    } catch (err) {
+        console.log(err)
+        res.status(401).json({status:'failed', message:err});
+    } */
 }
